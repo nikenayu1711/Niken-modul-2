accf22ce0a735a6c10c2b399e3c7300a8983e549
nikenayu1711 <82433473+nikenayu1711@users.noreply.github.com>
2021-04-14 20:08:14 +0700

    Create cd.java

---
+++ cd.java
@@ -0,0 +1,19 @@
+package javaapplication1;
+public class cd {
+    public String judul,publisher,kategori;
+    int stok;
+    String aktor;
+    String sutradara;
+    
+    public cd(String judul,String publisher, String kategori,int stok ){
+        this.judul=judul;
+        this.publisher=publisher;
+        this.stok=stok;
+        this.kategori=kategori;
+        
+    }
+    
+    cd(){
+        
+    }
+}
